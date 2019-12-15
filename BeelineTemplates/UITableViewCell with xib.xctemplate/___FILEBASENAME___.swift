3250d6572890d4b85cc10c6cf2b2e51c28bb591d
Vladimir Vysotsky <wwwvova1997@gmail.com>
2019-12-16 00:27:48 +0300

    updated template. added xib templates

---
+++ BeelineTemplates/UITableViewCell with xib.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  MyBeeline
+//
+//  Created by Vladimir on 03/08/2019.
+//  Copyright © 2019 Beeline. All rights reserved.
+//
+
+import MBUIKit
+
+final class ___FILEBASENAME___: UITableViewCell, ResetableView, TableViewRegisterable, NibLoadable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        resetToEmptyState()
+        initialSetup()
+    }
+    
+    func resetToEmptyState() {}
+}
+
+extension ___FILEBASENAME___ {
+    private func initialSetup() {}
+}
---
+++ BeelineTemplates/UIView with xib.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,24 @@
+//
+//  Test.swift
+//  MyBeeline
+//
+//  Created by Vladimir on 03/08/2019.
+//  Copyright © 2019 Beeline. All rights reserved.
+//
+
+import MBUIKit
+
+final class ___FILEBASENAME___View: UIView, NibLoadable, ResetableView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        resetToEmptyState()
+        initialSetup()
+    }
+    
+    func resetToEmptyState() {}
+}
+
+extension ___FILEBASENAME___View {
+    private func initialSetup() {}
+}
