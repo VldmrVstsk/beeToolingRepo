b71592e4ba87575aa8c6d26ef78365a2755767c4
Vladimir Vysotsky <wwwvova1997@gmail.com>
2019-12-18 17:06:17 +0300

    added new binding module

--- BeelineTemplates/RIB with UDF.xctemplate/___FILEBASENAME___Builder.swift
+++ BeelineTemplates/RIB with UDF.xctemplate/___FILEBASENAME___Builder.swift
@@ -5,20 +5,25 @@ import RIBs
 import UIAppTools
 
 final class ___VARIABLE_productName___Builder: Builder<EmptyDependency>, ___VARIABLE_productName___Buildable {
-  override init(dependency: EmptyDependency) {
-    super.init(dependency: dependency)
-  }
-
   func build(withListener listener: ___VARIABLE_productName___Listener) -> ___VARIABLE_productName___Routing {
+    // MARK: - Dependency
+
     let emptyComponent = EmptyComponent()
+
+    // MARK: - Instantiating
 
     let viewController = ___VARIABLE_productName___ViewController.instantiateFromStoryboard()
     let presenter = ___VARIABLE_productName___Presenter()
     let interactor = ___VARIABLE_productName___Interactor(presenter: presenter)
+    let router = ___VARIABLE_productName___Router(interactor: interactor, viewController: viewController)
 
-    VIPBinder.bind(view: viewController, interactor: interactor, presenter: presenter)
-
+    interactor.router = router
     interactor.listener = listener
-    return ___VARIABLE_productName___Router(interactor: interactor, viewController: viewController)
+
+    // MARK: - Binding
+    
+    VIPBinder.newBind(view: viewController, interactor: interactor, presenter: presenter)
+
+    return router
   }
 }
--- BeelineTemplates/RIB with UDF.xctemplate/___FILEBASENAME___Router.swift
+++ BeelineTemplates/RIB with UDF.xctemplate/___FILEBASENAME___Router.swift
@@ -2,9 +2,4 @@
 
 import RIBs
 
-final class ___VARIABLE_productName___Router: ViewableRouter<___VARIABLE_productName___Interactable, ___VARIABLE_productName___ViewControllable>, ___VARIABLE_productName___Routing {
-  override init(interactor: ___VARIABLE_productName___Interactable, viewController: ___VARIABLE_productName___ViewControllable) {
-    super.init(interactor: interactor, viewController: viewController)
-    interactor.router = self
-  }
-}
+final class ___VARIABLE_productName___Router: ViewableRouter<___VARIABLE_productName___Interactable, ___VARIABLE_productName___ViewControllable>, ___VARIABLE_productName___Routing {}
